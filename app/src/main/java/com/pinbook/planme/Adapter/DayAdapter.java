815b2af4a225b5a1be6bbd6513145c0486631877
farmstyle1 <f.natthapark@gmail.com>
2015-11-12 16:26:21 +0700

    Add day in Adapter

--- app/src/main/java/com/pinbook/planme/Adapter/DayAdapter.java
+++ app/src/main/java/com/pinbook/planme/Adapter/DayAdapter.java
@@ -4,25 +4,35 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
 
 import com.pinbook.planme.Fragment.FragmentDay;
 import com.pinbook.planme.MainActivity;
 
+import java.util.Calendar;
+
 
 public class DayAdapter extends FragmentStatePagerAdapter{
 
-    public static final String ARGS_POSITION = "name";
-    private final int PAGE_NUM = 31;
 
-    public DayAdapter(FragmentManager fm) {
+    public static final String ARGS_POSITION = "name";
+    int MaxDate = 0;
+    int Date = 0;
+
+
+    public DayAdapter(FragmentManager fm,int MaxDate,int Date) {
         super(fm);
+        this.MaxDate = MaxDate;
+        this.Date = Date;
+        Log.i("Check Adapter",String.valueOf(Date));
     }
 
     @Override
     public Fragment getItem(int position) {
         Fragment fragment = new FragmentDay();
         Bundle Arg = new Bundle();
-        Arg.putInt(ARGS_POSITION,position);
+        Arg.putInt(ARGS_POSITION,Date);
         fragment.setArguments(Arg);
 
         return fragment;
@@ -30,6 +40,16 @@ public class DayAdapter extends FragmentStatePagerAdapter{
 
     @Override
     public int getCount() {
-        return PAGE_NUM;
+
+        return MaxDate;
     }
+
+
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return "" + (position + 1);
+    }
+
+
 }
--- app/src/main/java/com/pinbook/planme/MainActivity.java
+++ app/src/main/java/com/pinbook/planme/MainActivity.java
@@ -5,8 +5,11 @@ import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
+
 import com.pinbook.planme.Adapter.DayAdapter;
 
+import java.util.Calendar;
 
 
 public class MainActivity extends FragmentActivity {
@@ -18,11 +21,22 @@ public class MainActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        // Get Day of Month & How many Day
+        Calendar c = Calendar.getInstance();
+        int monthMaxDays = c.getActualMaximum(Calendar.DAY_OF_MONTH);
+        int dayOfMonth = c.get(Calendar.DAY_OF_MONTH);
+       // Log.i("Check",String.valueOf(dayOfMonth));
+
         ViewPager viewPager = (ViewPager)findViewById(R.id.pager);
-        DayAdapter adapter = new DayAdapter(getSupportFragmentManager());
+        DayAdapter adapter = new DayAdapter(getSupportFragmentManager(),monthMaxDays,dayOfMonth);
         viewPager.setAdapter(adapter);
 
 
 
+
+
     }
+
+
 }
